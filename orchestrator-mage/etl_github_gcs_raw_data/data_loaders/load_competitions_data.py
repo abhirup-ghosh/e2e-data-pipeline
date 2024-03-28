df08230ecc9950c8448d65ae1e6d7e494ba82ba9
abhirup.ghosh <abhirup.ghosh@emmy-sharing.de>
2024-03-28 22:27:55 +0100

    added mage pipeline

---
+++ orchestrator-mage/etl_github_gcs_raw_data/data_loaders/load_competitions_data.py
@@ -0,0 +1,26 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://raw.githubusercontent.com/abhirup-ghosh/e2e-data-pipeline/main/data/competitions.csv'
+    response = requests.get(url)
+
+    return pd.read_csv(url, sep=',')
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
